cd4c2e8bf16a0044f048ce26033d63cdcf603727
Zrn-code <zxc0978827909@gmail.com>
2024-01-06 03:54:32 +0800

    add update password and delete account in profile settings

--- client/src/features/settings/profilesettings/index.js
+++ client/src/features/settings/profilesettings/index.js
@@ -87,39 +87,81 @@ function ProfileSettings() {
     return (
         <>
             {profile && profile.Username && (
-                <TitleCard title={`${profile.Username}'s Profile Settings`} topMargin="mt-2">
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                        <div className="form-control">
-                            <label className="label">
-                                <span>Gender</span>
-                            </label>
-                            <select
-                                value={profile.Gender}
-                                onChange={handleGenderChange}
-                                className="select block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                            >
-                                <option value="Male">Male</option>
-                                <option value="Female">Female</option>
-                            </select>
+                <div>
+                    <TitleCard title={`${profile.Username}'s Profile Settings`} topMargin="mt-2">
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span>Gender</span>
+                                </label>
+                                <select
+                                    value={profile.Gender}
+                                    onChange={handleGenderChange}
+                                    className="select block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                >
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                                </select>
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span>Birthday</span>
+                                </label>
+                                <input
+                                    type="date"
+                                    value={profile.Birthday}
+                                    onChange={handleBirthdayChange}
+                                    className="select block w-full  border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                />
+                            </div>
                         </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span>Birthday</span>
-                            </label>
-                            <input
-                                type="date"
-                                value={profile.Birthday}
-                                onChange={handleBirthdayChange}
-                                className="select block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                            />
+                        <div className="mt-5">
+                            <button className="btn btn-primary float-right" onClick={updateProfile}>
+                                Update
+                            </button>
                         </div>
-                    </div>
-                    <div className="mt-16">
-                        <button className="btn btn-primary float-right" onClick={updateProfile}>
-                            Update
-                        </button>
-                    </div>
-                </TitleCard>
+                    </TitleCard>
+                    <TitleCard title="Change Password" topMargin="mt-5">
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span>Old Password</span>
+                                </label>
+                                <input type="password" placeholder="Old Password" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span>New Password</span>
+                                </label>
+                                <input type="password" placeholder="New Password" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span>Check Password</span>
+                                </label>
+                                <input type="password" placeholder="Double" className="input input-bordered" />
+                            </div>
+                        </div>
+
+                        <div className="mt-5">
+                            <button className="btn btn-primary float-right">Change</button>
+                        </div>
+                    </TitleCard>
+                    <TitleCard title="Delete Account" topMargin="mt-5">
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span>Confirm Password</span>
+                                </label>
+                                <input type="password" placeholder="Password" className="input input-bordered" />
+                            </div>
+                        </div>
+
+                        <div className="mt-5">
+                            <button className="btn btn-primary float-right">Delete</button>
+                        </div>
+                    </TitleCard>
+                </div>
             )}
         </>
     )
